f7b363ac5a4fe1c1a26f13cd4c3edf53faf32b5a
ratul-space <to.ratul.hosen@gmail.com>
2023-12-26 14:44:15 +0600

    feat: add new composable to show a clickable text

--- app/src/main/java/com/example/jetpackcomposeintro/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeintro/MainActivity.kt
@@ -1,38 +1,47 @@
 package com.example.jetpackcomposeintro
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcomposeintro.ui.theme.JetpackComposeIntroTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Ratul")
+            Title()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Title() {
+    val context = LocalContext.current
+
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "test",
+        fontSize = 32.sp,
+        fontFamily = FontFamily.Cursive,
+        modifier = Modifier.clickable{
+            Toast.makeText(context, "test clicked!", Toast.LENGTH_LONG).show()
+        }
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    JetpackComposeIntroTheme {
-        Greeting("Ratul")
+fun DefaultPreview() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Title()
     }
 }
