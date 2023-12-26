e9b5e684fc45e7ecf687022c33db62751b907330
ratul-space <to.ratul.hosen@gmail.com>
2023-12-26 14:46:26 +0600

    feat: add color on the title text

--- app/src/main/java/com/example/jetpackcomposeintro/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeintro/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.ColorRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,6 +34,7 @@ fun Title() {
         text = "test",
         fontSize = 32.sp,
         fontFamily = FontFamily.Cursive,
+        color = Color.Companion.Green,
         modifier = Modifier.clickable{
             Toast.makeText(context, "test clicked!", Toast.LENGTH_LONG).show()
         }
