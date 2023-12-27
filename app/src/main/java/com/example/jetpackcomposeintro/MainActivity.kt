2b132ad960bb576bd43fddf72e8db7ad78a83b6a
ratul-space <to.ratul.hosen@gmail.com>
2023-12-27 12:46:45 +0600

    feat: uses modifier

--- app/src/main/java/com/example/jetpackcomposeintro/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeintro/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.annotation.ColorRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -23,6 +26,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -42,20 +48,67 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            MainContent()
 //            ContentTitle()
-            UserList()
+//            UserList()
         }
     }
 }
 
-@Composable
-fun UserList() {
-    Column(
-        modifier = Modifier.verticalScroll(rememberScrollState())
-    ) {
+data class User(
+    val id: Int
+)
 
-        for (i in 1..20) {
-            UserCard()
+@Composable
+fun MainContent() {
+    val user = User(1)
+    val users = remember { mutableStateListOf(user) }
+
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        UserList(users = users)
+        Button(modifier =
+        Modifier
+            .padding(24.dp)
+            .align(Alignment.BottomCenter),
+            onClick = {
+                users.add(User(1))
+
+            }) {
+            Text(text = "Add More")
+        }
+    }
+}
+
+//val users = mutableListOf(
+//    User(1),
+//    User(1),
+//    User(1),
+//    User(1),
+//    User(1),
+//    User(1),
+//    User(1),
+//    User(1),
+//
+//)
+@Composable
+fun UserList(users: List<User>) {
+//    val name = remember { mutableStateOf("Ratul") }
+
+
+//    Column(
+//        modifier = Modifier.verticalScroll(rememberScrollState())
+//    ) {
+//
+//        for (i in 1..20) {
+//            UserCard()
+//        }
+//    }
+    LazyColumn {
+        items(users) {
+            this@LazyColumn.items(users) { user ->
+                UserCard()
+            }
         }
     }
 }
@@ -96,7 +149,8 @@ fun UserCard() {
 @Composable
 fun DefaultPreview() {
     Surface(modifier = Modifier.fillMaxSize()) {
-        ContentTitle()
-        UserCard()
+//        ContentTitle()
+        MainContent()
+//        UserCard()
     }
 }
