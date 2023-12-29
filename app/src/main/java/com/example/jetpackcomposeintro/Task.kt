262e1e605d0a91ea8ed5641a8b4b9e89a0810956
ratul-space <to.ratul.hosen@gmail.com>
2023-12-29 11:48:18 +0600

    feat(task): add new composable task

---
+++ app/src/main/java/com/example/jetpackcomposeintro/Task.kt
@@ -0,0 +1,11 @@
+package com.example.jetpackcomposeintro
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Task(){
+    
+    Text(text = "Task")
+
+}
