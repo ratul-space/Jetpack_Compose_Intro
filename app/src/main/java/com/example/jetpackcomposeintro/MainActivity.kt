72553d028ed1b5f2ee790518b5dca9080851fec5
ratul-space <to.ratul.hosen@gmail.com>
2023-12-29 12:39:48 +0600

    refactor(MainActivity): optimize imports

--- app/src/main/java/com/example/jetpackcomposeintro/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeintro/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,9 +22,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
