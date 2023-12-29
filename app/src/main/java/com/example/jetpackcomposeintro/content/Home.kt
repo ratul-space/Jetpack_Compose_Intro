4c4244c3562c2504fd1aa2db25c43f0971d95f1f
ratul-space <to.ratul.hosen@gmail.com>
2023-12-29 12:16:18 +0600

    refactor(Home): optimize imports

--- app/src/main/java/com/example/jetpackcomposeintro/content/Home.kt
+++ app/src/main/java/com/example/jetpackcomposeintro/content/Home.kt
@@ -1,6 +1,5 @@
 package com.example.jetpackcomposeintro.content
 
-import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
